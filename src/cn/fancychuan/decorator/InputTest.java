5615687c1fe119b7825fa4db8a7e015b2586d331
fancyChuan <zxc7653>
2018-12-16 22:59:57 +0800

    装饰者模式：java I/O

---
+++ src/cn/fancychuan/decorator/InputTest.java
@@ -0,0 +1,19 @@
+package cn.fancychuan.decorator;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class InputTest {
+
+    public static void main(String[] args) throws Exception {
+        int c;
+        InputStream in = new LowerCaseInputStream(
+                new BufferedInputStream(new FileInputStream("test.txt")));
+
+        while ((c = in.read()) >= 0) {
+            System.out.print((char) c);
+        }
+        in.close();
+    }
+}
