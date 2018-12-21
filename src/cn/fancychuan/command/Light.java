746c66d064efc4fb7ee120d1f03accc29843165e
fancyChuan <12473750747@qq.com>
2018-12-21 08:17:13 +0800

    命令模式2

--- src/cn/fancychuan/command/Light.java
+++ src/cn/fancychuan/command/Light.java
@@ -1,12 +1,17 @@
 package cn.fancychuan.command;
 
 public class Light {
+    String location;
+
+    public Light(String where) {
+        this.location = where;
+    }
 
     public void on() {
-        System.out.println("灯亮啦~");
+        System.out.println(location + " 灯亮啦~");
     }
 
     public void off() {
-        System.out.println("关灯，我要睡觉哦饿了...");
+        System.out.println(location + " 关灯，我要睡觉哦饿了...");
     }
 }
