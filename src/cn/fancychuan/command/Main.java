64c1b30b8e3c14f9ee70764f6395d439f92e9732
fancyChuan <1247375074@qq.com>
2018-12-27 23:35:07 +0800

    工厂模式理解笔记

--- src/cn/fancychuan/command/Main.java
+++ src/cn/fancychuan/command/Main.java
@@ -1,5 +1,26 @@
 package cn.fancychuan.command;
 
+/**
+ * 20181226 命令模式
+ *
+ *      执行者只负责调用执行的命令，并不在乎要要调用的具体命令是啥
+ *
+ * 命令对象(接口)：execute() 负责执行请求、命令
+ *
+ * 每一个真实的命令对象实现接口的 execute() 并在方法体里面执行委托过来的真实对象，
+ * 再让真实对象去执行他本身的各种命令。 相当于真实对象
+ *
+ * 最后再把真是的命令对象传递给遥控器，由遥控器发起请求，执行命令。
+ *
+ * 核心思想其实就是把真实的对象层层封装，然后不断传到下一层。
+ * eg:
+ *          通过构造器              通过setCommand方法
+ *      Light -----> LightOnCommand --------> RemoteControl
+ *
+ * 更多用途：
+ *      1. 工作队列：队列类与对象完全解耦，比如上一秒做财务计算，下一秒读取网络数据
+ *      2. 日志记录：
+ */
 public class Main {
 
     public static void testSimpleControl() {
@@ -10,7 +31,6 @@ public class Main {
         remote.setCommand(lightOn);
         remote.buttonWasPressed();
     }
-
 
     public static void testRemoteControl() {
         RemoteControl remoteControl = new RemoteControl();
